74026452dcbdc0e3a5bb66ee39e05e2665799fff
김정현 <white20202@naver.com>
2023-11-14 09:40:53 +0900

    mypage

--- src/components/mypage/MyBookListCard.jsx
+++ src/components/mypage/MyBookListCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState} from "react";
 import styled from "styled-components";
 import { FaPlus } from "react-icons/fa";
 import { myBookList } from "../../data/mypagedata";
@@ -15,6 +15,13 @@ function MyBookListCard(props) {
     });
   };
 
+  const goaddbook = () => {
+    navigate("/search");
+  };
+
+  const MyBookListCard = (props) => {
+    const [completedBooks, setCompletedBooks] = useState([]);
+
   return (
     <BookListCardContainer>
       {/* 헤더 */}
@@ -22,35 +29,58 @@ function MyBookListCard(props) {
         <LeftBox>
           <LeftBoxText>저는 해리포터 좋아해요!</LeftBoxText>
         </LeftBox>
-        <AddBookButton>
+        <AddBookButton onClick={() => {goaddbook();}}>
           <FontAwesomeIcon icon={faPlus} />책 추가
         </AddBookButton>
       </BookListCardHeader>
       {/* 책 리스트 */}
       <BookListBodyContainer>
-        {myBookList.map((book) => {
-          return (
-            <>
-              <BookimgBox
-                src={book.img}
-                onClick={() =>
-                  goDetailPage(
-                    book.id,
-                    book.img,
-                    book.title,
-                    book.contents,
-                    book.author,
-                    book.publisher
-                  )
-                }
-              />
-            </>
-          );
-        })}
+        {myBookList.map((book) => (
+          <BookItem key={book.id}>
+            <BookimgBox
+              src={book.img}
+              onClick={() =>
+                goDetailPage(
+                  book.id,
+                  book.img,
+                  book.title,
+                  book.contents,
+                  book.author,
+                  book.publisher
+                )
+              }
+            />
+            <BookButtonsContainer>
+              <ActionButton
+                completed={completedBooks.includes(book.id)}
+                onClick={() => handleButtonClick(book.id)}
+              >
+                {completedBooks.includes(book.id) ? '독서완료' : '독서중'}
+              </ActionButton>
+              <ActionButton
+                completed={completedBooks.includes(book.id)}
+                onClick={() => handleButtonClick(book.id)}
+              >
+                {completedBooks.includes(book.id) ? '거래불가능' : '거래가능'}
+              </ActionButton>
+            </BookButtonsContainer>
+          </BookItem>
+        ))}
       </BookListBodyContainer>
       <BookListBodyContainer></BookListBodyContainer>
     </BookListCardContainer>
   );
+  function handleButtonClick(bookId) {
+    // bookId가 completedBooks에 있는지 확인
+    if (completedBooks.includes(bookId)) {
+      // 이미 있으면 제거
+      setCompletedBooks(completedBooks.filter((id) => id !== bookId));
+    } else {
+      // 없으면 추가
+      setCompletedBooks([...completedBooks, bookId]);
+    }
+  }
+};
 }
 
 export default MyBookListCard;
@@ -67,7 +97,7 @@ const BookListCardHeader = styled.div`
 
 const LeftBox = styled.div`
   position: relative;
-  background: #37d15d;
+  background: #5F749F;
   border-radius: 1em;
   padding: 1em 5em;
   height: 10px;
@@ -79,7 +109,7 @@ const LeftBox = styled.div`
     width: 0;
     height: 0;
     border: 1.719em solid transparent;
-    border-right-color: #37d15d;
+    border-right-color: #5F749F;
     border-left: 0;
     border-bottom: 0;
     margin-top: -0.959em;
@@ -137,4 +167,35 @@ const BookimgBox = styled.img`
   height: 140px;
   margin-right: 15px;
   cursor: pointer;
+`;
+
+const BookItem = styled.div`
+  display: flex;
+`;
+
+const BookButtonsContainer = styled.div`
+  display: flex;
+  width: 100px; 
+  flex-direction: column;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
+const ActionButton = styled.button`
+  border-radius: 5px;
+  background-color: ${({ completed }) => (completed ? '#5F749F' : '#fff')};
+  color: ${({ completed }) => (completed ? '#fff' : '#000')};
+  cursor: pointer;
+  margin-top: 10px;
+  padding: 5px;
+  border: 1px solid #000;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 700;
+  transition: background-color 0.3s, color 0.3s;
+
+  &:hover {
+    background-color: ${({ completed }) => (completed ? '#5F749F' : '#000')};
+    color: ${({ completed }) => (completed ? '#fff' : '#fff')};
+  }
 `;
